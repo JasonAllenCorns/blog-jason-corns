c2b1f11911958f1766aeb5d9106329769413b1d6
j.corns <jason.corns@classmates.com>
2016-03-01 13:49:17 -0800

    adds foundation to the mix.

---
+++ assets/javascripts/scripts.js
@@ -0,0 +1,16 @@
+//
+// Foundation-Jekyll - Javascripts
+// Author: @aaronkwhite
+//
+
+console.log('======================================== :: scripts');
+console.log("(j.corns) window", window);
+console.log("(j.corns) $(document)", $(document));
+console.log("(j.corns) $(document).foundation", $(document).foundation);
+console.log('======================================== :: end scripts');
+
+
+$(function(){ $(document).foundation(); });
+
+// Init Highlight.js
+//hljs.initHighlightingOnLoad();
